dc62592d682556dda1bb39e825b87fa7beb58b5f
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-04-26 20:39:32 +0200

    Add project files.

---
+++ MvvmlightBehaviors/MainWindowVM.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using System.ComponentModel;
+
+namespace MvvmlightBehaviors
+{
+
+    public class Person : ObservableObject
+    {
+        private string _firstName;
+        public string FirstName { get => _firstName; set => Set(() => FirstName, ref _firstName, value); }
+    }
+
+    public class MainWindowVM : ViewModelBase
+    {
+        private BindingList<Person> _persons = new BindingList<Person>();
+        public BindingList<Person> Persons { get => _persons; set => Set(() => Persons, ref _persons, value); }
+        public MainWindowVM()
+        {
+            loadData();
+        }
+
+    private void loadData()
+    {
+            Persons.Add(new Person() { FirstName = "NameA" });
+            Persons.Add(new Person() { FirstName = "NameB" });
+            Persons.Add(new Person() { FirstName = "NameC" });
+
+        }
+
+    }
+}
